bc8ce1fb677afc0a78e4a1c252ab2acd68627018
Simon Somlai <info@truetech.be>
2018-02-17 21:09:13 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">UPLOADING TO AMAZON EC2!</h1>
+          <h1 className="App-title">Simon is geweldig! Eline ook wel, maar een beetje minder :)</h1>
         </header>
       </div>
     );
