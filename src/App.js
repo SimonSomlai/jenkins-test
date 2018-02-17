b1e47be07c2eb92aca235f2949f9fb7aab77e984
Simon Somlai <info@truetech.be>
2018-02-17 12:33:56 +0100

    ready

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">s</h1>
+          <h1 className="App-title">UPLOADING TO AMAZON EC2!</h1>
         </header>
       </div>
     );
